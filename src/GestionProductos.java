2bc1e5115f971ec73c853a729c2425d46d71ff7a
Ariel Macias <materchico@gmail.com>
2025-05-08 09:52:38 -0500

    Macias Ariel: Implementacion de las validaciones de los loggers

--- src/GestionProductos.java
+++ src/GestionProductos.java
@@ -1,2 +1,25 @@
+
+
 public class GestionProductos {
+
+
+    // Metodos para implementar "private boolean esValida(int) y private void log(String)"
+    // Validacion para que la cantidad interna no sea negativa
+    private boolean esValida(int cantidad) {
+        return cantidad >= 0;
+    }
+
+    // Creacion de un logger sencillo
+    private void log(String msg) {
+        System.out.println("[LOG]" + msg);
+    }
+
+    // Para obtener la cantidad actual
+    public int getCantidadActual() {
+        return getCantidadActual();
+    }
+
+
+
+
 }
