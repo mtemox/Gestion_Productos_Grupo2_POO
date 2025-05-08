f9d23047072ee4ff513308b7af5e274ca638dc56
POO <josselyn.ayoharo@epn.edu.ec>
2025-05-08 09:49:21 -0500

    Josselyn Ayo

--- src/GestionProductos.java
+++ src/GestionProductos.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+import static java.rmi.server.LogStream.log;
+
 public class GestionProductos {
+    private String nombre;
+    private int cantidadActual;
+    public GestionProductos(String nombre, int cantidadInicial){
+        this.nombre = nombre;
+        this.cantidadActual = Math.max(0, cantidadInicial);
+        log(" Productos" + nombre+ " Creado con cantidad inicial: " + this.cantidadActual);
+
+
+    }
 }
